dff386325432570004d705a7d4f22f70532fd492
n3v3rlov1ing <desynq@mail.ru>
2023-01-23 19:30:16 +0300

    Some reworked

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from requests import get 
 
 r = get('https://www.cbr-xml-daily.ru/daily_json.js').json()
-def convert(cur : int, valute : str) -> int:
+
+def convert(cur : int, valute : str) -> str:
     try:
         if r['Valute'][valute]['Nominal'] > 1:
             return cur * r['Valute'][valute]['Value']  / r['Valute'][valute]['Nominal']
         res = cur * r['Valute'][valute]['Value'] 
-        return res
+        return f'{cur} {valute} в рублях будет: {res} руб.'
     except (ValueError, KeyError):
         return 'Ошибка'
 
