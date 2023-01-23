8fe785258b7566303a88d10595c0e298bc788513
n3v3rlov1ing <desynq@mail.ru>
2023-01-23 18:59:51 +0300

    some reworked

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from requests import get
 r = get('https://www.cbr-xml-daily.ru/daily_json.js').json()
 def convert(cur : int, valute : str) -> int:
     try:
+        if r['Valute'][valute]['Nominal'] > 1:
+            return cur * r['Valute'][valute]['Value']  / r['Valute'][valute]['Nominal']
         res = cur * r['Valute'][valute]['Value'] 
         return res
     except (ValueError, KeyError):
